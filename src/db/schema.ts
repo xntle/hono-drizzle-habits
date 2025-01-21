2e524360697fefcedea22000077cd53985651f31
An Le <115895584+xntle@users.noreply.github.com>
2025-01-21 02:25:10 -0800

    feat: new app

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,10 @@
 import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
 
-// Habits Table
+// babits table
 export const habits = pgTable("habits", {
-  id: serial("id").primaryKey(), // Unique ID for each habit
-  title: text("title").notNull(), // Title of the habit
-  description: text("description"), // Description of the habit
-  streak: integer("streak").default(0), // Current streak count (default 0)
-  createdAt: timestamp("created_at").defaultNow(), // Timestamp when the habit was created
+  id: serial("id").primaryKey(), 
+  title: text("title").notNull(), 
+  description: text("description"), 
+  streak: integer("streak").default(0), 
+  createdAt: timestamp("created_at").defaultNow(), 
 });
